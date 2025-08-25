eb42e6d4de0385a00049a00f471903503dc8669b
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-08-26 01:29:06 +0400

    fearure/adminjs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { createAppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const AppModule = await createAppModule();
+  const server = express();
+
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.enableCors();
+
   await app.listen(process.env.PORT ?? 3001);
+  console.log(`🚀 AdminJS: http://localhost:${process.env.PORT ?? 3001}/admin`);
 }
 bootstrap();
